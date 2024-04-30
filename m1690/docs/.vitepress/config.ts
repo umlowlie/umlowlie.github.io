4c2e99defc481290fc7cb95fc1a83df19e8b04df
umlowlie <noumena.lonewolf@gmail.com>
2024-04-30 13:32:47 +0300

    m1690 for slion check config change

--- m1690/docs/.vitepress/config.ts
+++ m1690/docs/.vitepress/config.ts
@@ -8,7 +8,8 @@ const APP_BASE_PATH = basename(process.env.GITHUB_REPOSITORY || '')
 
 export default defineConfig({
   outDir: '../dist',
-  base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+//   base: APP_BASE_PATH ? `/${APP_BASE_PATH}/` : '/',
+   base: `/m1690/`,
 
   lang: 'ru-RU',
   title: 'M1690',
